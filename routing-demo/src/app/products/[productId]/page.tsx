0ba2ac7e4117500b7d55e15e4cd40f715eb96716
Raghuveer <raghuveerp25@gmail.com>
2025-03-25 18:56:24 +0530

    Progamatically trigger not found page

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,13 +1,18 @@
 //props is an object that has params property;
 //params is a promise that returns an object with property productId;
 
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: Promise<{ productId: string }>;
 };
 
 const ProductDetails = async (props: Props) => {
   const params = await props.params;
-  const productId = params.productId;
+  const productId = Number(params.productId);
+  if (productId > 1000) {
+    notFound();
+  }
   return <h1>Product Details of Product {productId}</h1>;
 };
 export default ProductDetails;
